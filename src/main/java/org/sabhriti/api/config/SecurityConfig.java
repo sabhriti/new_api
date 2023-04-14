b66ee69a7c1d7506333c168921f03912ba117df6
Pawan Bhattarai <pawan.bhattarai@devk.de>
2023-04-14 16:01:13 +0200

    feat: finalizes the login and registration.

--- src/main/java/org/sabhriti/api/config/SecurityConfig.java
+++ src/main/java/org/sabhriti/api/config/SecurityConfig.java
@@ -7,21 +7,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
-    private final AuthenticationManager authenticationManager;
+    private final static List<String> UNSECURED_PATHS = List.of(
+            "/security/login/**",
+            "/security/signup/**",
+            "/security/create-password/**",
+            "/security/forget-password/**",
+            "/user-token/validate/**",
+            "/v2/api-docs",
+            "/swagger-ui**",
+            "/swagger-resources/**",
+            "/*/swagger-resources/**",
+            "/*/v2/api-docs"
+    );
 
+    private final AuthenticationManager authenticationManager;
     private final SecurityContextRepository securityContextRepository;
 
     @Bean
     SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
-        return http.cors().disable()
+        var authorizeExchangeSpec = http.cors().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint((swe, e) -> Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)))
                 .accessDeniedHandler((swe, e) -> Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN)))
@@ -29,19 +45,11 @@ public class SecurityConfig {
                 .csrf().disable()
                 .authenticationManager(authenticationManager)
                 .securityContextRepository(securityContextRepository)
-                .authorizeExchange()
-                .pathMatchers("/security/login/**").permitAll()
-                .pathMatchers("/security/signup/**").permitAll()
-                .pathMatchers("/security/create-password/**").permitAll()
-                .pathMatchers("/security/forget-password/**").permitAll()
-                .pathMatchers("/user-token/validate/**").permitAll()
-                .pathMatchers(
-                        "/v2/api-docs",
-                        "/swagger-ui**",
-                        "/swagger-resources/**",
-                        "/*/swagger-resources/**",
-                        "/*/v2/api-docs"
-                ).permitAll()
+                .authorizeExchange();
+
+        UNSECURED_PATHS.forEach(path -> authorizeExchangeSpec.pathMatchers(path).permitAll());
+
+        return authorizeExchangeSpec
                 .pathMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyExchange().authenticated()
                 .and()
